9fa49f0e0cdc16915f2ffe3774f2f3b09cebb782
Pratiksha Tivale <pratikshativale05@gmail.com>
2026-06-26 10:54:02 +0530

    Add app icons (192 & 512px), update favicon and SW cache

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'expense-tracker-v1';
+const CACHE_NAME = 'expense-tracker-v2';
+
 const urlsToCache = [
   '/',
-  '/static/js/bundle.js',
-  '/static/css/main.css',
-  '/manifest.json'
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 // Install event
@@ -22,7 +23,7 @@ self.addEventListener('fetch', (event) => {
         // Return cached version or fetch from network
         return response || fetch(event.request);
       }
-    )
+      )
   );
 });
 
